c52bc1dab7eaec4499690775732a4759d8660118
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-14 22:45:31 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,23 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const corsOptions: CorsOptions = {
-  //   origin: ['http://localhost:3000', 'https://sayido.lk', 'https://wedding-directory-ten.vercel.app'.'https://wedding-directory-git-test-deploy-hirushans-projects-02f6fa2b.vercel.app/'], // Allow only your Next.js app to access the API
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true, // Allow cookies to be sent with requests
-  //   optionsSuccessStatus: 204, // Some legacy browsers choke on 204
-  // };
+  const corsOptions: CorsOptions = {
+    origin: [
+      'http://localhost:3000',
+      'https://sayido.lk',
+      'https://sayido-eta.vercel.app',
+      'https://sayido.duckdns.org',
+      'https://vendor-rats-leaving-constitutional.trycloudflare.com',
+      'https://wedding-directory-ten.vercel.app',
 
-  app.enableCors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-})
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
+    optionsSuccessStatus: 204,
+  };
+
+   app.enableCors(corsOptions);
  const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0');
   console.log(`Server running on port ${port}`);}
